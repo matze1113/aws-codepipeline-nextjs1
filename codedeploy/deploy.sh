a1170f2ced0ed900411e03591781d56822261658
tarasowski <dimitri+github@tarasowski.de>
2024-02-17 08:54:28 +0100

    fix: sudo issues

--- codedeploy/deploy.sh
+++ codedeploy/deploy.sh
@@ -4,6 +4,6 @@ cd /var/www/myapp
 #npm install
 #npm run build
 export PORT=80
-rm -rf node_modules package-lock.json
-npm install
+sudo rm -rf node_modules package-lock.json
+sudo npm install
 nohup npm run start > output.log 2>&1 &
